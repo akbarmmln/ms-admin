cb0fa46f13ca7f88808ad923557c376a52ac2ed7
crazycoding <contoh@crazycoding.com>
2021-02-13 21:12:48 +0700

    update code

--- config/db.js
+++ config/db.js
@@ -52,6 +52,8 @@ const sequelize = new Sequelize(settings.dbname, settings.username, settings.pas
     idleTimeoutMillis: 3000,
     evict: 30000
   },
+  logging: logger.debug(),
+  logging: true,
   timezone: '+07:00'
 });
 
