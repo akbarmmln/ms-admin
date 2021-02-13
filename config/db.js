e9fedc59508e2d01a757abd1a1fa331bd31131f4
crazycoding <contoh@crazycoding.com>
2021-02-13 21:46:59 +0700

    update code

--- config/db.js
+++ config/db.js
@@ -54,7 +54,7 @@ const sequelize = new Sequelize(settings.dbname, settings.username, settings.pas
   },
   logging: console.log,                  // Default, displays the first parameter of the log function call
   logging: (sql, queryObject) => console.log(sql), // Displays all log function call parameters
-  // logging: false,                        // Disables logging
+  logging: false,                        // Disables logging
   // logging: msg => logger.debug(msg),     // Use custom logger (e.g. Winston or Bunyan), displays the first parameter
   // logging: logger.debug.bind(logger),
   timezone: '+07:00'
